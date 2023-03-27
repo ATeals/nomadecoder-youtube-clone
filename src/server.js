8eca244528a4f906af412fa9e81c16396e15bb31
ateals <ateals@icloud.com>
2023-03-27 17:08:08 +0900

    get메서드 사용!

--- src/server.js
+++ src/server.js
@@ -4,4 +4,17 @@ const PORT = 4000;
 
 const app = express(); 
 
+app.get("/", (req, res)=>{ 
+    return res.send("hello sever!");
+});
+
+app.get("/login", (req, res)=>{ 
+    return res.send("THis page is login page!!");
+});
+
+
 app.listen(PORT, ()=> console.log(`server listening on http://localhost:4000/ 👻`));
+
+
+
+ 
