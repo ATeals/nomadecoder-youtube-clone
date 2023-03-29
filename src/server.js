e176f2dbd046c9f2001235cb19a5c1f88ba744a4
ateals <ateals@icloud.com>
2023-03-29 17:47:32 +0900

    install morgan

--- src/server.js
+++ src/server.js
@@ -1,29 +1,10 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express(); 
 
-const logger = (req, res, next) =>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
 
-const privateMiddleware = (req, res, next)=>{
-    const url = req.url;
-    if(url ==="/protected"){
-        return res.send("<h1> Not Allowd</h1>");
-    }
-    next();
-}
-
-app.use(logger);
-app.use(privateMiddleware);
-
-
-app.get("/protected", (req, res)=>{
-    return res.send("Welcome to private room!");
-});
+app.use();
 
 app.get("/", (req, res)=>{ 
     return res.send("hello sever!");
@@ -32,6 +13,3 @@ app.get("/", (req, res)=>{
 
 app.listen(PORT, ()=> console.log(`server listening on http://localhost:4000/ 👻`));
 
-
-
- 
