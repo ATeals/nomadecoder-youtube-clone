e64445e7f1b26cb03be6294fb4acd02e341ec9fe
ateals <ateals@icloud.com>
2023-03-29 18:29:37 +0900

    (#4.1) router

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 
     Html, Css, Javascript, Node.js, Express, MongoDB
 
-    유튜브 모듈 구현
+
+
+### 구현 페이지
+    / -> home
+    /join -> join
+    /login -> login
+    /search -> search
+
+    /users/edit -> edit user
+    /users/delete -> delete user
+
+    /videos/watch -> watch video
+    /videos/edit -> edit video
+    /videos/delete -> delete video
+    /videos/comments -> comment on a video
+    /videos/comments/delete -> delete a video
--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express(); 
 
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
 
-app.use();
+app.use("/", globalRouter);
+app.use("/vidios", videoRouter);
+app.use("/users", userRouter);
 
-app.get("/", (req, res)=>{ 
-    return res.send("hello sever!");
-});
+
 
 
 app.listen(PORT, ()=> console.log(`server listening on http://localhost:4000/ 👻`));
