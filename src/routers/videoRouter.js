0bdecba573b6d98a82919933fd08ba4c4ecc2fa4
ateals <ateals@icloud.com>
2023-04-08 16:47:14 +0900

    mongodb 설치!

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {watch, getEdit, posetEdit} from "../controllers/videoController";
+import {watch, getEdit, posetEdit, getUpload, postUpload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(posetEdit)
-
+videoRouter.route("/upload").get(getUpload).post(postUpload)
 
 
 export default videoRouter;
