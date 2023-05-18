e50cbe8af25a93516ad68ebeb65cdf35025bbffc
ateals <ateals@icloud.com>
2023-05-18 14:38:48 +0900

    edit, delete

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,18 +6,41 @@ export const home = async (req, res) => {
     res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-    res.render("watch", { pageTitle: `Watching` });
+    const video = await Video.findById(id);
+
+    if (!video) {
+        return res.render("404", { pageTitle: "Not Found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing ` });
+    const video = await Video.findById(id);
+
+    if (!video) {
+        return res.render("404", { pageTitle: "Not Found" });
+    }
+
+    return res.render("edit", { pageTitle: `Edit ${video.title} `, video });
 };
-export const posetEdit = (req, res) => {
+export const posetEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+
+    if (!video) {
+        return res.render("404", { pageTitle: "Not Found" });
+    }
+
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
+
     return res.redirect(`/videos/${id}`);
 };
 
@@ -31,7 +54,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     } catch (error) {
@@ -40,4 +63,14 @@ export const postUpload = async (req, res) => {
             error: error._message,
         });
     }
+};
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.render("404", { pageTitle: "Not Found" });
+    }
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,11 @@
 import express from "express";
-import {watch, getEdit, posetEdit, getUpload, postUpload} from "../controllers/videoController";
+import { watch, getEdit, posetEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(posetEdit)
-videoRouter.route("/upload").get(getUpload).post(postUpload)
-
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(posetEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
-
-
