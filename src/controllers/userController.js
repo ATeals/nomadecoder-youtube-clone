7e182ee12245bf01dd96750c5442e434dca43cbb
ateals <ateals@icloud.com>
2023-05-18 16:27:42 +0900

    user join + bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,21 @@
+import User from "../models/User";
 
-export const join = (req, res)=> res.send("join");
-export const edit = (req, res)=> res.send("Edit User");
-export const remove = (req, res)=> res.send("Remove User");
-export const login = (req, res)=> res.send("Login");
-export const logout = (req, res)=> res.send("Logout");
-export const see = (req, res)=> res.send("See user");
-
-
-
+export const getJoin = (req, res) => {
+    return res.render("user/createAccount", { pageTitle: "Create Account" });
+};
+export const postJoin = async (req, res) => {
+    const { email, name, username, password, location } = req.body;
+    await User.create({
+        email,
+        name,
+        username,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
+export const see = (req, res) => res.send("See user");
--- src/init.js
+++ src/init.js
@@ -1,6 +1,8 @@
-import app from "./server";
 import "./db";
 import "./models/Video";
+import "./models/User";
+
+import app from "./server";
 
 const PORT = 4000;
 
