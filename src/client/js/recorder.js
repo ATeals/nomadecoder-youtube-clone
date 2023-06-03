0bd77c7132206fbaa9e6a10b4f9480ce935728e3
ateals <ateals@icloud.com>
2023-06-03 19:40:08 +0900

    스크롤 css

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -4,35 +4,35 @@ const video = document.querySelector(".preview");
 let stream;
 let recorder;
 
-// const handleStart = () => {
-//     startBtn.innerText = "Stop Recording";
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
 
-//     recorder = new MediaRecorder(stream);
-//     recorder.ondataavailable = (e) => {
-//         const videoFile = URL.createObjectURL(e.data);
-//         console.log(videoFile);
-//         video.srcObject = null;
-//         video.src = videoFile;
-//         video.play();
-//     };
-//     recorder.start();
-//     setTimeout(() => {
-//         recorder.stop();
-//     }, 5000);
-// };
-// const handleStop = () => {
-//     startBtn.innerText = "Start Recording";
-// };
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        const videoFile = URL.createObjectURL(e.data);
+        console.log(videoFile);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.play();
+    };
+    recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 5000);
+};
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+};
 
-// const init = async () => {
-//     stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
-//     video.srcObject = stream;
-//     video.play();
-// };
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+    video.srcObject = stream;
+    video.play();
+};
 
-// init();
+init();
 
-// startBtn.addEventListener("click", () => {
-//     if (startBtn.innerText === "Start Recording") handleStart();
-//     else handleStop();
-// });
+startBtn.addEventListener("click", () => {
+    if (startBtn.innerText === "Start Recording") handleStart();
+    else handleStop();
+});
