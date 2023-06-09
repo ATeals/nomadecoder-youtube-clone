31756068461d3a42af1ddcc83a092102d6aee6bd
ateals <ateals@icloud.com>
2023-06-09 13:38:06 +0900

    커밋

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -192,7 +192,7 @@ export const postChangePassword = async (req, res) => {
     user.password = newPassword;
     await user.save();
 
-    return res.redirect("/users/logout");
+    return res.redirect("/logout");
 };
 
 export const profile = async (req, res) => {
